e547cb6bdd4c9150715b62be9243bfe76d34534d
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-09-03 23:45:26 -0300

    Add the exercise 04 of flow controle expressions solution refatored with 'while functions' for validate inputs

--- trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/expressao_repeticao/src/lista02_expressao_repeticao_while/Atividade04.java
+++ trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/expressao_repeticao/src/lista02_expressao_repeticao_while/Atividade04.java
@@ -7,132 +7,92 @@ public class Atividade04 {
 
 	public static void main(String[] args) {
 		/*
-		 *Uma empresa desenvolveu uma pesquisa interna para conhecer 
-		 *os colaboradores da área de Desenvolvimento e precisam de um
-		 *sistema para analisar os dados. Escreva um algoritmo em Java,
-		 *que leia via teclado as seguintes informações de cada 
-		 *colaborador:
+		 * Uma empresa desenvolveu uma pesquisa interna para conhecer os colaboradores
+		 * da área de Desenvolvimento e precisam de um sistema para analisar os dados.
+		 * Escreva um algoritmo em Java, que leia via teclado as seguintes informações
+		 * de cada colaborador:
 		 *
-		 *- Idade (Número inteiro)
-		 *- Sexo (Número Inteiro): 
-		 *	 1 – Masculino
-		 *	 2 – Feminino
-		 *	 3 – Outros
-		 *- Categoria (Número Inteiro):
-		 *	 1 – Backend
-		 *	 2 – Frontend
-		 *	 3 – Mobile
-		 *	 4 – FullStack
+		 * - Idade (Número inteiro) 
+		 * - Sexo (Número Inteiro): 1 – Masculino 2 – Feminino 3 – Outros 
+		 * - Categoria (Número Inteiro): 1 – Backend 2 – Frontend 3 – Mobile  4 – FullStack
 		 *
-		 *Após digitar a categoria, o sistema deverá perguntar ao 
-		 *usuário se ele deseja continuar a leitura dos dados de um 
-		 *novo colaboradore ou não (S/N). Caso seja pressionada a tecla
-		 * N, mostre na tela: 
+		 * Após digitar a categoria, o sistema deverá perguntar ao usuário se ele deseja
+		 * continuar a leitura dos dados de um novo colaboradore ou não (S/N). 
+		 * Caso seja pressionada a tecla N, mostre na tela: 
 		 * 
-		 * 
-		 * - O número de pessoas desenvolvedoras Backend
-     	 * - O número de mulheres desenvolvedoras Frontend
-		 * - O número de homens desenvolvedores Mobile maiores de 40 anos
+		 * - O número de pessoas desenvolvedoras Backend 
+		 * - O número de mulheres desenvolvedoras Frontend 
+		 * - O número de homens desenvolvedores Mobile maiores de 40 anos 
 		 * - O número de mulheres desenvolvedoras FullStack menores de 30 anos
-         */
-		
+		 */
+
 		Locale.setDefault(Locale.US);
-		Scanner leia = new Scanner(System.in); 
-		
-		String confirmacao;
-		int idade,genero,categoria,PrimeiraAcumulador = 0,SegundoAcumulador=0,TerceiroAcumulador=0,QuartoAcumulador=0;
-		
-		
-		
-		System.out.print("\nDigite a Idade: ");
-		idade=leia.nextInt();
-		
-		System.out.println("1 – Masculino"
-				           + "\n2 – Feminino" 
-				           + "\n3 – Outros ");
-		
-		System.out.print("\nDigite o Sexo: ");
-		genero = leia.nextInt();
-		
-		System.out.println("1 – Backend"
-							+ "\n2 – Frontend"
-							+ "\n3 – Mobile"
-							+ "\n4 – FullStack");
-		
-		System.out.print("\nDigite a Categoria: ");
-		categoria = leia.nextInt();
-		
-		System.out.println("\nDeseja continuar (S/N): ");
-		confirmacao = leia.next();
-		
-		
-	
-		
-		
-		while(confirmacao.equalsIgnoreCase("S")) {
-			
-			if(categoria == 1) { //Total de pessoas desenvolvedoras Backend:
-				PrimeiraAcumulador++;
-			}
-			
-			if(genero == 2 && categoria == 2) { //Total de mulheres desenvolvedoras Frontend: 
-				SegundoAcumulador++;
-			}
-			
-			if(idade > 40 && genero == 1 && categoria ==3) { //Total de homens desenvolvedores Mobile maiores de 40 anos:
-				TerceiroAcumulador++;
-			}
-			
-			if((idade < 30 && genero == 2) && categoria == 4) { //Total de mulheres desenvolvedoras FullStack menores de 30 anos:
-				QuartoAcumulador++;
-			}
-			
+		Scanner leia = new Scanner(System.in);
+
+		String confirmacao = "S"; // inicializado a varíavel com "S" para poder utilizar o loop while
+		int idade, genero, categoria, PrimeiraAcumulador = 0, SegundoAcumulador = 0, TerceiroAcumulador = 0,
+				QuartoAcumulador = 0;
+
+		while (confirmacao.equalsIgnoreCase("S")) {
+
 			System.out.print("\nDigite a Idade: ");
 			idade = leia.nextInt();
 			
-			System.out.println("\n1 – Masculino"
-					+ "\n2 – Feminino" 
-					+ "\n3 – Outros ");
+			// validação de idade
+			while (idade < 1 || idade > 120) {
+				System.out.print("\nIdade inválida. Digite novamente: ");
+				idade = leia.nextInt();
+			}
+
 			
-			System.out.print("Digite o Sexo: ");
-			genero  =leia.nextInt();
+			// validação de gênero
+			System.out.print("\nDigite o genero (1 – Masculino 2 – Feminino 3 – Outros): ");
+			genero = leia.nextInt();
+			while(genero < 1 || genero > 3) {
+				System.out.print("\nGênero inválido. Digite novamente: ");
+				genero = leia.nextInt();
+			}
+
 			
-			System.out.println("\n1 – Backend"
-					+ "\n2 – Frontend"
-					+ "\n3 – Mobile"
-					+ "\n4 – FullStack");
-			System.out.print("Digite a Categoria: ");
+			// validação de categoria
+			System.out.print("\nDigite a Categoria: (1 – Backend 2 – Frontend 3 – Mobile 4 – FullStack): ");
 			categoria = leia.nextInt();
+			while (categoria < 1 || categoria > 4) {
+				System.out.print("\nDigite a Categoria: (1 – Backend 2 – Frontend 3 – Mobile 4 – FullStack): ");
+				categoria = leia.nextInt();
+
+			}
+
 			
-			System.out.print("\nDeseja continuar (S/N): ");
+			System.out.println("\nDeseja continuar (S/N): ");
 			confirmacao = leia.next();
-			
-			if(categoria == 1) { //Total de pessoas desenvolvedoras Backend:
+
+			if (categoria == 1) { // Total de pessoas desenvolvedoras Backend:
 				PrimeiraAcumulador++;
 			}
-			
-			if(genero == 2 && categoria == 2) { //Total de mulheres desenvolvedoras Frontend: 
+
+			if (genero == 2 && categoria == 2) { // Total de mulheres desenvolvedoras Frontend:
 				SegundoAcumulador++;
 			}
-			
-			if(idade > 40 && genero == 1 && categoria ==3) { //Total de homens desenvolvedores Mobile maiores de 40 anos:
+
+			if (idade > 40 && genero == 1 && categoria == 3) { // Total de homens desenvolvedores Mobile maiores de 40
+																// anos:
 				TerceiroAcumulador++;
 			}
-			
-			if((idade < 30 && genero == 2) && categoria == 4) { //Total de mulheres desenvolvedoras FullStack menores de 30 anos:
+
+			if ((idade < 30 && genero == 2) && categoria == 4) { // Total de mulheres desenvolvedoras FullStack menores
+																	// de 30 anos:
 				QuartoAcumulador++;
 			}
-			
+
 		}
-		
-		System.out.println("Total de pessoas desenvolvedoras Backend: " + PrimeiraAcumulador);
-		System.out.println("Total de mulheres desenvolvedoras Frontend: " + SegundoAcumulador);
-		System.out.println("Total de homens desenvolvedores Mobile maiores de 40 anos: " + TerceiroAcumulador);
-		System.out.println("Total de mulheres desenvolvedoras FullStack menores de 30 anos: " + QuartoAcumulador);
-		
-		
+
+		System.out.println("\nTotal de pessoas desenvolvedoras Backend: " + PrimeiraAcumulador);
+		System.out.println("\nTotal de mulheres desenvolvedoras Frontend: " + SegundoAcumulador);
+		System.out.println("\nTotal de homens desenvolvedores Mobile maiores de 40 anos: " + TerceiroAcumulador);
+		System.out.println("\nTotal de mulheres desenvolvedoras FullStack menores de 30 anos: " + QuartoAcumulador);
+
 		leia.close();
-	
 
 	}
 
