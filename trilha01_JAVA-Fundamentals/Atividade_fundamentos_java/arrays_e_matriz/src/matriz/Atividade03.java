1215a5c38aeae0b9fc1c837e3e00bd288a46e78b
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-09-06 01:36:59 -0300

    Add exercise04 of Java matriz

--- trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/arrays_e_matriz/src/matriz/Atividade03.java
+++ trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/arrays_e_matriz/src/matriz/Atividade03.java
@@ -6,39 +6,84 @@ import java.util.Scanner;
 public class Atividade03 {
 	public static void main(String[] args) {
 		/*
-		 * Elabore um algoritmo que leia uma Matriz 3x3 
-		 * de números inteiros e em seguida, mostre na tela:
+		 * Elabore um algoritmo que leia uma Matriz 3x3 de números inteiros e em
+		 * seguida, mostre na tela:
 		 * 
-		 *-Todos os elementos da Diagonal Principal
-		 *-Todos os elementos da Diagonal Secundária
-		 *-A Soma de todos os elementos da Diagonal Principal
-		 *-A Soma de todos os elementos da Diagonal Secundária
+		 * -Todos os elementos da Diagonal Principal -Todos os elementos da Diagonal
+		 * Secundária -A Soma de todos os elementos da Diagonal Principal -A Soma de
+		 * todos os elementos da Diagonal Secundária
 		 */
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
-		
-		
+
 		int matriz[][] = new int[3][3];
-		int input;
+		int tamanho = matriz.length;
+		int diagPrincip[] = new int[tamanho];
+		int diagSecund[] = new int[tamanho];
+		int somaDiagPrincip = 0;
+        int somaDiagSecund = 0;
+        boolean calcDiagPrincip = true;
 		
-		for(int linha = 0; linha<3;linha++) {
-			for(int coluna = 0;coluna<3;coluna++) {
-				System.out.print("Digite ujm valor para a posição ["+linha+"]["+coluna+"]: ");
+		
+
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.print("Digite ujm valor para a posição [" + linha + "][" + coluna + "]: ");
 				matriz[linha][coluna] = leia.nextInt();
 			}
 		}
-		
-		for(int linha = 0; linha<3;linha++) {
-			for(int coluna = 0;coluna<3;coluna++) {
-				System.out.println("O valor armazenado na posição: ["+linha+"]["+coluna+"]: "+matriz[linha][coluna]);
-				
+
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.println("[" + linha + "][" + coluna + "]: " + matriz[linha][coluna]);
+
 			}
 		}
+
+		// percorre os elementos da diagonal principal
+		for (int i = 0; i < tamanho; i++) {
+			diagPrincip[i] = matriz[i][i];
+			
+			
+		}
+
+		System.out.println("Elementos da Diagonal Principal: ");
+		for (int i = 0; i < tamanho; i++) {
+			System.out.print(diagPrincip[i] + " ");
+		}
+
+		// percorre os elementos da diagonal principal
+		for(int i = 0; i < tamanho; i++) {
+			diagSecund[i] = matriz[(tamanho-1-i)][i];
+		}
+		
+		// Imprimime a diagonal secundária
+		System.out.println("Elementos da Diagonal Secundária: ");
+		for(int i = 0; i < tamanho; i++) {
+			System.out.print(diagSecund[i]+ " ");	
+		}
+		
+		for (int i = 0; i < tamanho; i++) {
+			somaDiagPrincip += matriz[i][i];
+
+            if (calcDiagPrincip) {
+            	somaDiagSecund += matriz[i][tamanho - 1 - i];
+            }
+
+            
+            if (i == tamanho - 1) {
+            	calcDiagPrincip = false;
+            }
+        }
 		
 		
+		System.out.println("Soma dos elementos da Diagonal Principal: " + somaDiagPrincip);
+        System.out.println("Soma dos elementos da Diagonal Secundária: " + somaDiagSecund);
 		
 		
+		leia.close();
+
 	}
 
 }
--- trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/arrays_e_matriz/src/matriz/Atividade04.java
+++ trilha01_JAVA-Fundamentals/Atividade_fundamentos_java/arrays_e_matriz/src/matriz/Atividade04.java
@@ -1,8 +1,70 @@
 package matriz;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Atividade04 {
 	public static void main(String[] args) {
+		/*
+		 * Elabore um algoritmo que leia as notas de uma classe com 10 participantes
+		 * durante 4 bimestres de um ano. As 4 notas de cada participante serão
+		 * armazenadas em uma linha de uma matriz de números reais, logo cada linha da
+		 * matriz serão as notas de um participante. Em um vetor de números reais,
+		 * armazene as médias de cada participante e exiba as médias de cada um na tela.
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+
+		final int alunes = 10;
+		final int bimestres = 4;
+		float validNota = 0;
+		float matrizNotas[][] = new float[alunes][bimestres];
+		float media[] = new float[alunes];
 		
+		
+
+		System.out.println("Quadro de notas:");
+		for (int i = 0; i < alunes; i++) {
+			System.out.println("Digite as notas do alune " + (i + 1) + ":");
+			for (int j = 0; j < bimestres; j++) {
+				System.out.print("Bimestre " + (j + 1) + ": ");
+				matrizNotas[i][j]= leia.nextFloat();
+				if ( validNota < 0 || validNota > 10) {
+                    System.out.println("Nota inválida. Insira uma nota entre 0 e 10.");
+                    j--; 
+                } else {
+                	matrizNotas[i][j] = validNota;
+                }
+			}
+		}
+		
+		
+		for (int i = 0; i < alunes; i++) {
+            float somaNotas = 0;
+            for (int j = 0; j < bimestres; j++) {
+                somaNotas += matrizNotas[i][j];
+            }
+            media[i] = somaNotas / bimestres;
+        }
+		
+		
+		System.out.println("Médias dos participantes:");
+        for (int i = 0; i < alunes; i++) {
+            System.out.println("Participante " + (i + 1) + ": " + media[i]);
+        }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		leia.close();
+
 	}
 
 }
